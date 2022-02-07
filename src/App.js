a826583332a3d456251deb066f283b9f24f7357a
Guillaume Ader <gader@groupon.com>
2022-02-07 20:04:11 +0000

    feat: wip start refactor using arrays

--- src/App.js
+++ src/App.js
@@ -9,21 +9,21 @@ function App() {
     const { key, keyCode } = e;
     console.warn(keyCode, key);
     // if hit enter to validate word
-    if (keyCode === 13 && word.length === 5) {
+    if (keyCode === 13 && word[wordIndex].length === 5) {
       // todo
       wordIndex ++;
     }
     // delete last char
     if (keyCode === 8) {
-      const max = word.length;
-      setWord(word.slice(0, max - 1))
+      const max = word[wordIndex].length;
+      setWord([word[wordIndex].slice(0, max - 1)])
     }
     // not regular char
     if (!key.trim() || key.length > 1 || !alphabet.includes(key)) return
     // stop adding char to word if length is 5
-    if (word.length === 5) return 
+    if (word[wordIndex].length === 5) return 
 
-    setWord(word + key)
+    setWord([word + key])
     console.warn(word)
   }
   useEffect(() => {
@@ -32,7 +32,7 @@ function App() {
       window.removeEventListener('keydown', handleKeyevent)
     }
   })
-  const [word, setWord] = useState('')
+  const [word, setWord] = useState([''])
   let wordIndex = 0;
   const rows = Array(6).fill(null)
   return (
@@ -41,7 +41,7 @@ function App() {
     >
       <h1>Wordle</h1>
       <h2>{word[wordIndex]}</h2>
-      {rows.map((row, index) => <Grid key={index} currentWord={word} />)}
+      {rows.map((row, index) => <Grid key={index} currentWord={word[wordIndex]} />)}
 
     </div>
   );
