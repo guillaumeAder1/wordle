a411a3c611fdf4f4230a04bcad632ac1cbb63c20
Guillaume Ader <gader@groupon.com>
2022-02-07 20:53:58 +0000

    feat: :fire: works with array :)

--- src/App.js
+++ src/App.js
@@ -5,43 +5,45 @@ import { useEffect, useState } from 'react';
 function App() {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz'
   function handleKeyevent (e) {
-    console.warn(word)
     const { key, keyCode } = e;
-    console.warn(keyCode, key);
+    // console.warn(keyCode, key);
     // if hit enter to validate word
-    if (keyCode === 13 && word[wordIndex].length === 5) {
+    if (keyCode === 13 && word[lastIndex].length === 5) {
       // todo
-      wordIndex ++;
+      setWord([...word, ''])
     }
     // delete last char
     if (keyCode === 8) {
-      const max = word[wordIndex].length;
-      setWord([word[wordIndex].slice(0, max - 1)])
+      const max = word[lastIndex].length;
+      setWord([word[lastIndex].slice(0, max - 1)])
     }
     // not regular char
     if (!key.trim() || key.length > 1 || !alphabet.includes(key)) return
     // stop adding char to word if length is 5
-    if (word[wordIndex].length === 5) return 
-
-    setWord([word + key])
-    console.warn(word)
+    if (word[lastIndex].length === 5) return 
+    
+    word[lastIndex] += key
+    setWord([...word])
   }
+  const [word, setWord] = useState([''])
+  const [lastIndex, setLastIndex] = useState(0)
+  const rows = Array(6).fill(null)
+  useEffect(() => {
+    setLastIndex(word.length - 1);
+  }, [word])
   useEffect(() => {
     window.addEventListener('keydown', handleKeyevent)
     return () => {
       window.removeEventListener('keydown', handleKeyevent)
     }
   })
-  const [word, setWord] = useState([''])
-  let wordIndex = 0;
-  const rows = Array(6).fill(null)
   return (
     <div
       className="App"
     >
       <h1>Wordle</h1>
-      <h2>{word[wordIndex]}</h2>
-      {rows.map((row, index) => <Grid key={index} currentWord={word[wordIndex]} />)}
+      <h2>{word[lastIndex]}</h2>
+      {rows.map((row, index) => <Grid key={index} currentWord={word[index] || []} />)}
 
     </div>
   );
--- src/Grid.js
+++ src/Grid.js
@@ -5,9 +5,6 @@ function Grid({ currentWord }) {
     const boxes = Array(5).fill(null)
     return (
         <div>
-            {/* <div>
-                { currentWord.slice(0, 5) }
-            </div> */}
             <div className='box-row'>
                 {boxes.map((box, i) => (
                     <Box
