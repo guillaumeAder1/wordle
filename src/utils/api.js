19fce47892ea2ac943a24be72a871fced963eae6
Guillaume Ader <gader@groupon.com>
2022-02-11 10:54:37 +0000

    feat: default placehodler

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,6 @@
 
 export async function fetchNewWord() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-  return response.json();
+  // const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  // return response.json();
+  return { newWord: 'toast' }
 }
