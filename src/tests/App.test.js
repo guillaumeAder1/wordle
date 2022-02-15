4542a58f6984d61029c4282ad6ebff4fa65a52e2
Guillaume Ader <gader@groupon.com>
2022-02-15 13:08:07 +0000

    test: add test a helper test function

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from '../App';
-import { typeWord } from './utils/utils'
+import { typeWord, isWordInGrid } from './utils/utils'
 import { fetchNewWord } from '../utils/api'
 
 jest.mock('../utils/api')
@@ -12,36 +12,47 @@ beforeEach(() => {
 
 
 
-test('app render', () => {
+test('app render title, keyboard and grid elements... TODO', () => {
   const linkElement = screen.getByText(/wordle/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-test('user input should be displayed and removed with keyboard event', async () => {
-  // add char to grid
-  fireEvent.keyDown(window, { key: 'a' })
-  screen.getByRole('cell', { name: 'a' })
-  fireEvent.keyDown(window, { key: 'b' })
-  screen.getByRole('cell', { name: 'b' })
-  // delete last char
-  fireEvent.keyDown(window, { key: 'Backspace', keyCode: 8 })
-  screen.getByRole('cell', { name: 'a' })
-  expect(screen.queryByRole('cell', { name: 'b' })).toBeNull()
-  fireEvent.keyDown(window, { key: 'Backspace', keyCode: 8 })
-  expect(screen.queryByRole('cell', { name: '1' })).toBeNull()
-})
-test('should validate the row when there is 5 char and should add corresponding class color', async () => { 
-  // add char to grid
-  typeWord('toask')
-  screen.getByRole('cell', { name: 't' })
-  screen.getByRole('cell', { name: 'o' })
-  screen.getByRole('cell', { name: 'a' })
-  screen.getByRole('cell', { name: 's' })
-  screen.getByRole('cell', { name: 'k' })
-  // hit enter key to validate the row
-  await fireEvent.keyDown(window, { keyCode: 13 })
-  // screen.debug(
-  //   screen.getAllByRole('row')[0]
-  // )
-  expect(document.querySelectorAll('.box.green').length).toBe(4)
+describe('User input', () => {
+  test('user input should be displayed and removed with keyboard event', async () => {
+    // add char to grid
+    fireEvent.keyDown(window, { key: 'a' })
+    screen.getByRole('cell', { name: 'a' })
+    fireEvent.keyDown(window, { key: 'b' })
+    screen.getByRole('cell', { name: 'b' })
+    // delete last char
+    fireEvent.keyDown(window, { key: 'Backspace', keyCode: 8 })
+    screen.getByRole('cell', { name: 'a' })
+    expect(screen.queryByRole('cell', { name: 'b' })).toBeNull()
+    fireEvent.keyDown(window, { key: 'Backspace', keyCode: 8 })
+    expect(screen.queryByRole('cell', { name: '1' })).toBeNull()
+  })
+  test('should validate the row when there is 5 char and should add corresponding class color', async () => { 
+    // add char to grid
+    typeWord('toask')
+    expect(isWordInGrid('toask').length).toBe(5)
+    // hit enter key to validate the row
+    await fireEvent.keyDown(window, { keyCode: 13 })
+    expect(document.querySelectorAll('.box.green').length).toBe(4)
+  })
+  test('should add next row when user keeps typing', () => { 
+    typeWord('qwert')
+    fireEvent.keyDown(window, { keyCode: 13 })
+    typeWord('yuiop')
+    fireEvent.keyDown(window, { keyCode: 13 })
+    typeWord('asdfg')
+    fireEvent.keyDown(window, { keyCode: 13 })
+    typeWord('hjklz')
+    fireEvent.keyDown(window, { keyCode: 13 })
+    typeWord('xcvbn')
+    fireEvent.keyDown(window, { keyCode: 13 })
+    expect(isWordInGrid('qwert').length).toBe(5)
+    expect(isWordInGrid('yuiop').length).toBe(5)
+    expect(isWordInGrid('asdfg').length).toBe(5)
+    expect(isWordInGrid('hjklz').length).toBe(5)
+    expect(isWordInGrid('xcvbn').length).toBe(5)
+  })
 })
--- src/tests/utils/utils.js
+++ src/tests/utils/utils.js
@@ -1,4 +1,9 @@
-import { fireEvent } from "@testing-library/react"
+import { fireEvent, screen } from "@testing-library/react"
+
 export const typeWord = word => { 
   word.split('').forEach(char => fireEvent.keyDown(window, { key: char }))
 }
+// return all occurences of char from {word}
+export const isWordInGrid = word => word.split('')
+  .map(char => screen.queryByRole('cell', { name: char }))
+  .filter(Boolean)
